456618d7a4fbdcc772d41afc9f71af30ba485d46
vanashimko <vana.shimko@gmail.com>
2017-09-13 01:01:31 +0300

    Improve drawing

--- drawer.py
+++ drawer.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Button, Slider
 
 
-def draw_plots(title, signal, params_builder, variable_parameter):
-    plt.gcf().canvas.set_window_title(title)
-    for phase in variable_parameter:
-        plt.plot(list(signal(params_builder(phase), 512)))
-    plt.show()
+def draw_plots(title, signal, params_builder, variable_parameter):    
+    for parameter_value in variable_parameter:
+        plt.plot(list(signal(params_builder(parameter_value), 512)), label='${}={}$'.format(title, round(parameter_value, 2)))
+    plt.legend(loc='center left')
 
 
 def draw_polyharmonic(signal, params):
@@ -16,13 +15,11 @@ def draw_polyharmonic(signal, params):
         phases = list(map(lambda x: x.val, sliders))
         new_params = [x._replace(**{'phase':phases[i]}) for i, x in enumerate(params)]
         plot.set_ydata(list(signal(new_params, 512)))
-        figure.canvas.draw_idle()
 
     def reset(_):
         for slider in sliders:
             slider.reset()
 
-    figure, _ = plt.subplots()
     plt.subplots_adjust(bottom=0.3)
 
     plot, = plt.plot(list(signal(params, 512)))
@@ -35,8 +32,8 @@ def draw_polyharmonic(signal, params):
     sliders = []
     for i, param in enumerate(params):
         axes = plt.axes([0.17, 0.1+i*0.03, 0.65, 0.01])
-        slider = Slider(axes, 'f={}'.format(param.frequency), 0, pi*2, valinit=param.phase)
+        slider = Slider(axes, '$f={}$'.format(param.frequency), 0, pi*2, valinit=param.phase)
         slider.on_changed(update)
         sliders.append(slider)
 
-    plt.show()
+    return sliders + [button]
--- main.py
+++ main.py
@@ -6,29 +6,30 @@ import drawer
 from signals import HarmonicParameters, harmonic_signal, polyharmonic_signal
 
 
-def main():
-    # show_phases()
-    # show_frequencies()
-    # show_amplitudes()
-    show_polyharmonic()
+def main():    
+    results = []
+    for i, viewer in enumerate([show_phases, show_frequencies, show_amplitudes, show_polyharmonic]):
+        plt.subplot(2, 2, i + 1)
+        results.append(viewer())    
+    plt.show()
 
 
 def show_phases():
     phases = [0, pi, pi/6, pi/4, pi/2]
     params = lambda x: HarmonicParameters(amplitude=10, phase=x, frequency=2)
-    drawer.draw_plots('Phases', harmonic_signal, params, phases)
+    drawer.draw_plots(r'\phi', harmonic_signal, params, phases)
 
 
 def show_frequencies():
     params = lambda x: HarmonicParameters(amplitude=3, phase=pi/2, frequency=x)
     frequencies = [5, 4, 2, 6, 3]
-    drawer.draw_plots('Frequences', harmonic_signal, params, frequencies)
+    drawer.draw_plots(r'f', harmonic_signal, params, frequencies)
 
 
 def show_amplitudes():
     params = lambda x: HarmonicParameters(amplitude=x, phase=pi/2, frequency=1)
     amplitudes = [2, 3, 6, 5, 1]
-    drawer.draw_plots('Amplitudes', harmonic_signal, params, amplitudes)
+    drawer.draw_plots(r'a', harmonic_signal, params, amplitudes)
 
 
 def show_polyharmonic():
@@ -39,10 +40,7 @@ def show_polyharmonic():
         (1, 2*pi, 4),
         (1, pi, 5)
     ]))
-    drawer.draw_polyharmonic(polyharmonic_signal, params)
-    # print(params)
-    # plt.plot(list(polyharmonic_signal(params, 512)))
-    # plt.show()
+    return drawer.draw_polyharmonic(polyharmonic_signal, params)
 
 
 if __name__ == '__main__':
